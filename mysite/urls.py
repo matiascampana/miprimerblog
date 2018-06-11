3b1ffdfb4a196bdc47959e2758429d666a5e1149
matiascampana <matias_campana_y@hotmail.com>
2018-06-11 14:59:36 -0400

    con la url de blog

--- mysite/urls.py
+++ mysite/urls.py
@@ -4,8 +4,9 @@ from django.contrib import admin
 urlpatterns = [
     #Examples:
     #url(r'^$', 'mysite.views.home', name='home'),
-    #url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^admin/', (admin.site.urls)),
+    url(r'^blog/', ('blog.urls')),
 
 ]
